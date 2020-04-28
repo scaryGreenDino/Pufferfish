7d60f018e68adf6eac6a1a1f3aee13f26742f664
Jordan Coffland <Jordan.c@aws.com>
2020-04-28 11:34:34 -0500

    A base to start off of

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-// Your First C++ Program
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!";
+    //--------------------------Setup Files---
+    string key;
+    string inputFileName;
+    string outputFileName;
+    cout << "Please enter your key: " << endl;
+    cin >> key;
+    cout << "Please enter input file: " << endl;
+    cin >> inputFileName;
+    ifstream in;
+    in.open(inputFileName);
+    char c = in.get();
+    while (in.good())
+    {
+        cout << c;
+        c = in.get();
+    }
+    in.close();
     return 0;
 }
-//this is comment
