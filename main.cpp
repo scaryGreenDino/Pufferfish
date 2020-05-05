793279dfeb40ed368e7cc7928562ba357cc53004
Jordan Coffland <Jordan.c@aws.com>
2020-05-04 19:00:02 -0500

    DONE!?

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 typedef union key {
     char read[2];
-    short bytes = 0;
+    unsigned short bytes = 0;
 };
 char *P[] = {"49fe", "d3c6", "7326", "1234", "defb", "5a8b", "1e61", "77ad", "94b2", "5731"};
 char *S[8][4] = {{"abcb", "7386", "6ea3", "4128"}, {"689b", "22c7", "532d", "a42b"}, {"0e52", "f025", "8724", "9ac0"}, {"776a", "629f", "4510", "3188"}, {"1111", "da99", "52bc", "dead"}, {"deaf", "beef", "5810", "7219"}, {"733b", "5ac7", "2468", "9753"}, {"8263", "820d", "e72f", "410c"}};
@@ -42,7 +42,6 @@ char *xorb(char *a, char *b)
 
     ak.read[0] = a[0];
     ak.read[1] = a[1];
-
     bk.read[0] = b[0];
     bk.read[1] = b[1];
     ak.bytes = ak.bytes ^ bk.bytes;
@@ -50,30 +49,57 @@ char *xorb(char *a, char *b)
     a[1] = ak.read[1];
     return a;
 }
-char f(char *a)
+void f(char *a, char *fa)
 {
-    return *a;
+    union key ak;
+    ak.read[0] = a[0];
+    ak.read[1] = a[1];
+    unsigned short tmp = 0;
+    unsigned short right = 0;
+    for (int c = 0; c < 8; c++)
+    {
+        right = (ak.bytes >> ((7 - c) * 2)) % 4;
+        tmp += (unsigned short)stoi(S[c][right], NULL, 16);
+    }
+    ak.bytes = tmp;
+    fa[0] = ak.read[0];
+    fa[1] = ak.read[1];
 }
 
-char *encrypt(char *a, char *b, int c)
+char *encrypt(char *ca, char *cb, int c, union key keys[])
 {
+
+    union key a;
+    union key b;
+    a.read[0] = ca[0];
+    a.read[1] = ca[1];
+    b.read[0] = cb[0];
+    b.read[1] = cb[1];
+
     if (c == 8)
     {
-        a = xora(a, P[9]);
-        b = xora(b, P[8]);
+        a.bytes = a.bytes ^ keys[9].bytes;
+        b.bytes = b.bytes ^ keys[8].bytes;
 
-        return concat(a, b);
+        // a = xora(a, P[9]);
+        // b = xora(b, P[8]);
+
+        return concat(a.read, b.read);
     }
-    a = xora(a, P[c]);
-    char *fa = new char[2];
-    *fa = f(a);
-    b = xorb(fa, b);
+    // a = xora(a, P[c]);
+    a.bytes = a.bytes ^ keys[c].bytes;
+    union key fa;
+    f(a.read, fa.read);
+    b.bytes = fa.bytes ^ b.bytes;
+    // b = xorb(fa, b);
     c++;
-    return encrypt(b, a, c);
+    return encrypt(b.read, a.read, c, keys);
 }
 int main()
 {
-
+    unsigned short test = 65535;
+    test = test + 2;
+    cout << test << endl;
     //--------------------------Setup Files---
     // char *read = &r.read;
     // char *key = &k.read;
@@ -81,6 +107,21 @@ int main()
     char *b = new char[2];
     // key[0] = 'a';
     // key[1] = 'b';
+    union key keys[10];
+    union key temp;
+    string t;
+    cout << "Please enter key: ";
+    cin >> t;
+    const char *keyIn = t.c_str();
+    unsigned short pc;
+    for (int c = 0; c < 10; c++)
+    {
+        temp.read[0] = keyIn[c % (strlen(keyIn))];
+        temp.read[0] = keyIn[(c + 1) % (strlen(keyIn))];
+
+        pc = stoi(P[c], NULL, 16);
+        keys[c].bytes = pc ^ temp.bytes;
+    }
     string inputFileName;
     string outputFileName = "output.txt";
     // char *read = new char[2];
@@ -125,7 +166,7 @@ int main()
         b[0] = buffer[c + 2];
         b[1] = buffer[c + 3];
 
-        char *encrypted = encrypt(a, b, 0);
+        char *encrypted = encrypt(a, b, 0, keys);
         printf("%c%c%c%c", encrypted[0], encrypted[1], encrypted[2], encrypted[3]);
         _sleep(1000);
     }
